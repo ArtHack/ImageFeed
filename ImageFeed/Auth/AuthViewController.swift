cf82d85472082a1b00f6f725c64e3d7a56f88c77
Art Hack <art.hack.ios@gmail.com>
2023-02-24 17:32:48 +0300

    sprint_10 WebView.

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by arthack on 22.02.2023.
+//
+
+import UIKit
+
+
+final class AuthViewController: UIViewController {
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+}
---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by arthack on 20.02.2023.
+//
+
+import Foundation
+
+let AccessKey = "dPl3i6n7ELzPVvq55BF1Tayc_oxYZewxTy54JrtEtoY"
+let SecretKey = "_tU17nB6r8dbPVm9J4Y7_PmFqquu7-SWvnpUaW7vLBE"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
