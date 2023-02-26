827f563e83865b89d218cb1bc0818e3854d972a2
Art Hack <art.hack.ios@gmail.com>
2023-02-26 19:05:36 +0300

    Sprint_10. KVO.

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -9,5 +9,24 @@ import UIKit
 
 
 final class AuthViewController: UIViewController {
-    private let ShowWebViewSegueIdentifier = "ShowWebView"
+    private let showWebViewSegueIdentifier = "ShowWebView"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showWebViewSegueIdentifier {
+            guard let webViewViewController = segue.destination as?
+                    WebViewViewController
+            else { fatalError("Failed to prepare for \(showWebViewSegueIdentifier)")}
+            webViewViewController.delegate = self
+        }else {
+                super.prepare(for: segue, sender: sender)
+        }
+    }
+}
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
+    }
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
 }
